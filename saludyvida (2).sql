3ebbc7f082282bcc34d3fc82fa6b6d99ec34b1f8
bruno7890 <66882478+bruno7890@users.noreply.github.com>
2024-05-03 22:39:24 -0500

    Add files via upload
    base de datos

---
+++ saludyvida (2).sql
@@ -0,0 +1,189 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 04-05-2024 a las 03:37:55
+-- Versión del servidor: 8.2.0
+-- Versión de PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `saludyvida`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `actividades`
+--
+
+DROP TABLE IF EXISTS `actividades`;
+CREATE TABLE IF NOT EXISTS `actividades` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_cliente` int DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `peso_inicio` decimal(5,2) DEFAULT NULL,
+  `peso_fin` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_cliente` (`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Disparadores `actividades`
+--
+DROP TRIGGER IF EXISTS `actualiza_peso_kg`;
+DELIMITER $$
+CREATE TRIGGER `actualiza_peso_kg` AFTER INSERT ON `actividades` FOR EACH ROW BEGIN
+    DECLARE last_weight DECIMAL(5, 2);
+    -- Obtener el peso_fin más reciente del cliente
+    SELECT peso_fin INTO last_weight FROM Actividades WHERE id_cliente = NEW.id_cliente ORDER BY fecha DESC LIMIT 1;
+    -- Actualizar el peso_kg del cliente en la tabla de Clientes
+    UPDATE Clientes SET peso_kg = last_weight WHERE id = NEW.id_cliente;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `apellidos` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `edad` int DEFAULT NULL,
+  `estatura` decimal(4,2) DEFAULT NULL,
+  `peso_kg` decimal(5,2) DEFAULT NULL,
+  `direccion` varchar(100) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `correo` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `telefono` varchar(15) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `fecha_inicio_inscripcion` date DEFAULT NULL,
+  `fecha_final_inscripcion` date DEFAULT NULL,
+  `estado` tinyint(1) DEFAULT '1',
+  `id_usuario` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `correo` (`correo`),
+  KEY `id_usuario` (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `nombre`, `apellidos`, `edad`, `estatura`, `peso_kg`, `direccion`, `correo`, `telefono`, `fecha_inicio_inscripcion`, `fecha_final_inscripcion`, `estado`, `id_usuario`) VALUES
+(1, 'Juan', 'Pérez', NULL, NULL, NULL, '', 'juan@example.com', '1234567890', NULL, NULL, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reportes`
+--
+
+DROP TABLE IF EXISTS `reportes`;
+CREATE TABLE IF NOT EXISTS `reportes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_cliente` int DEFAULT NULL,
+  `id_actividad` int DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `tipo_periodo` enum('Diario','Semanal','Mensual') COLLATE utf8mb4_spanish_ci NOT NULL,
+  `perdio_peso` enum('Si','No') COLLATE utf8mb4_spanish_ci NOT NULL,
+  `archivo_pdf` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_cliente` (`id_cliente`),
+  KEY `id_actividad` (`id_actividad`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rutinas`
+--
+
+DROP TABLE IF EXISTS `rutinas`;
+CREATE TABLE IF NOT EXISTS `rutinas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `id_actividad` int DEFAULT NULL,
+  `duracion` time DEFAULT NULL,
+  `distancia_recorrida` decimal(6,2) DEFAULT NULL,
+  `calorias` int DEFAULT NULL,
+  `tipo_ejercicio` varchar(50) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_actividad` (`id_actividad`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` text CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  `usuario` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  `perfil` text CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  `foto` text COLLATE utf8mb4_spanish_ci NOT NULL,
+  `estado` int NOT NULL,
+  `fecha` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `ultimo_login` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `usuario` (`usuario`(191))
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `usuario`, `password`, `perfil`, `foto`, `estado`, `fecha`, `ultimo_login`) VALUES
+(1, '', 'admin', '123', 'Administrador', '', 1, '2024-05-03 04:01:35', '2024-05-03 21:05:49'),
+(2, '', 'cliente1', '123', 'Cliente', '', 1, '2024-05-03 04:01:35', '2024-05-04 00:00:48');
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `actividades`
+--
+ALTER TABLE `actividades`
+  ADD CONSTRAINT `actividades_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`);
+
+--
+-- Filtros para la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD CONSTRAINT `clientes_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `reportes`
+--
+ALTER TABLE `reportes`
+  ADD CONSTRAINT `reportes_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`),
+  ADD CONSTRAINT `reportes_ibfk_2` FOREIGN KEY (`id_actividad`) REFERENCES `actividades` (`id`);
+
+--
+-- Filtros para la tabla `rutinas`
+--
+ALTER TABLE `rutinas`
+  ADD CONSTRAINT `rutinas_ibfk_1` FOREIGN KEY (`id_actividad`) REFERENCES `actividades` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
